d6e78e11a354799c6a198e4cf13d3e6f2bbece98
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-10-26 22:59:32 +0300

    redux data usage

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,15 @@
+import { useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Счётчик</h1>
-      <div className={classes.value}>0</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import redux from 'redux';
+import { legacy_createStore } from 'redux';
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
@@ -15,6 +15,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
   return state;
 };
 
-const store = redux.legacy_createStore(counterReducer);
+const store = legacy_createStore(counterReducer);
 
 export default store;
