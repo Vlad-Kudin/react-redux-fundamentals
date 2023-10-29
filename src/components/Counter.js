2f787a17ffcec730e66a60562dd15e6453debe52
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-10-29 17:50:47 +0200

    sending action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,17 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatchFunction = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatchFunction({type: "increment"});
+  };
+
+  const decrementHandler = () => {
+    dispatchFunction({type: "decrement"});
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +19,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Счётчик</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>-</button>
+        <button onClick={incrementHandler}>+</button>
+      </div>
       <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
     </main>
   );
