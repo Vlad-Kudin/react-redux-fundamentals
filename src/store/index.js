f7f1c9af178f513d4eaa7b0df9a3ce7907e89e63
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-11-02 15:52:10 +0200

    added redux-toolkit and slice for counter

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { legacy_createStore } from 'redux';
 
 const initialState = {
   counter: 0,
   isCounterInvisible: false
 }
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.number;
+    },
+    decrease(state, action) {
+      state.counter = state.counter - action.number;
+    },
+    setCounterVisibility(state) {
+      state.isCounterInvisible = !state.isCounterInvisible;
+    },
+  }
+})
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
